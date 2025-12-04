4392e85b36d2cd998148c61950ae84aade9d80ee
AlexGotham <aleger567@gmail.com>
2025-12-04 11:21:48 +0100

    estim_hpv

---
+++ src/estim_HPV.jl
@@ -0,0 +1,30 @@
+# Fonction Infection pour toutes souches et vaccin
+
+using DataFrames
+
+function estim_HPV(df_bool::DataFrame, W::Vector{Float64}, souche::Int64, target::Float64 )
+    A = [- 0.5, 1, 1.5, 2, -0.5, 1.5, 2.5, -0.5, 1, 1.5, 2, 2.5] # coeff de confusion fixé
+    cible = [6, 11, 16,18]
+
+    # Vaccin prend -10 si souche concernée, sinon 0
+    if souche in cible
+        coefVac = -10
+    else
+        coefVac = 0
+    end
+    coef = vcat(coefVac, W, A)
+    regression = Matrix(df_bool) * coef
+
+    # Calcul pour une prévalence 
+    β₀ = estim_b0.(target,regression)
+    prob_reg = sigmoid.(regression+β₀)
+
+    # Simulation des infections avec le rajout d'un intercept
+    HPV = rand.(Binomial.(1,prob_reg))
+
+    return HPV
+end
+
+
+#df_bool = DataFrame(rand(Bool, 100, 15),:auto)
+#print(estim_HPV(df_bool,[0.4,0.2],6,0.05))
