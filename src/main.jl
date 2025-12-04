e6eeb09f131c6aa348101d0f7481b7e3feff6d07
SergeSuuu <capitanserge73@gmail.com>
2025-12-04 10:37:38 +0100

    Confusion

--- src/main.jl
+++ src/main.jl
@@ -14,10 +14,11 @@ function borneSomme(vec_inf::Vector{Float64},vec_sup::Vector{Float64})
     return prop
 end
 
+#prop = borneSomme([16.5,70.9,7.2]/100,[20.7,75.5,9.4]/100)
 
 # Fonction qui généralise la simulation: 
 
-function simul_Var(mod_var::Vector{String}, prop_var::Vector{Float64}, tauxEvolution::Vector{Vector{Float64}}, n::Int64, proportion_age::Vector{Float64})
+function Estim_Confusion(mod_var::Vector{String}, prop_var::Vector{Float64}, tauxEvolution::Vector{Vector{Float64}}, n::Int64, proportion_age::Vector{Float64})
     # Population
     modalite_age = ["18-19","20-24","25-34","35-44"]
     prop_age = proportion_age
@@ -31,14 +32,12 @@ function simul_Var(mod_var::Vector{String}, prop_var::Vector{Float64}, tauxEvolu
     return vec
 end
 
-taux_age_sex = [[0.01,0.03 , 0.02, 0],[1,1.6,1,0.4],[1,1.4,1,0.6],[1,1.4,1,0.6]]
-propo_age = [0.06,0.18,0.37,0.39]
-moda_ageSexe = ["18-19","20-24","25-34","35-44"]
-propo_ageSexe = [0.1,0.2,0.4,0.3]
-n = 5
-simul_Var(moda_ageSexe,propo_ageSexe,taux_age_sex,n,propo_age)
-
-#prop = borneSomme([16.5,70.9,7.2]/100,[20.7,75.5,9.4]/100)
+# taux_age_sex = [[0.01,0.03 , 0.02, 0],[1,1.6,1,0.4],[1,1.4,1,0.6],[1,1.4,1,0.6]]
+# propo_age = [0.06,0.18,0.37,0.39]
+# moda_ageSexe = ["18-19","20-24","25-34","35-44"]
+# propo_ageSexe = [0.1,0.2,0.4,0.3]
+# n = 5
+# Estim_Confusion(moda_ageSexe,propo_ageSexe,taux_age_sex,n,propo_age)
 
 # Fonction sigmoïde
 
