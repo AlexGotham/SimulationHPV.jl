899bdcf4ad71c45c841b7e30355351558e2eaa65
SergeSuuu <capitanserge73@gmail.com>
2025-12-09 12:37:06 +0100

    Examples Fonctionnels

--- Exemple/exemple.jl
+++ Exemple/exemple.jl
@@ -1,7 +1,7 @@
 using SimulationHPV
 
 # Fonction sigmoïde
-print(SimulationHPV.sigmoid.([12.0,12.0,130]))
+print(sigmoid.([12.0,12.0,130]))
 
 # Fonction Confusion
 taux_age_sex = [[0.01,0.03 , 0.02, 0],[1,1.6,1,0.4],[1,1.4,1,0.6],[1,1.4,1,0.6]]
@@ -9,20 +9,20 @@ propo_age = [0.06,0.18,0.37,0.39]
 moda_ageSexe = ["18-19","20-24","25-34","35-44"]
 propo_ageSexe = [0.1,0.2,0.4,0.3]
 n = 5
-SimulationHPV.estim_Confusion(moda_ageSexe,propo_ageSexe,taux_age_sex,n,propo_age)
+estim_Confusion(moda_ageSexe,propo_ageSexe,taux_age_sex,n,propo_age)
 
 # Fonction Estim_Vaccin
-df_bool_vaccin = DataFrame(rand(Bool, 100, 12),:auto)
+df_bool_vaccin = DataFrame(rand(Bool, 100, 16),:auto)
 prop_vaccin_simul = 0.55
-print(SimulationHPV.estim_Vaccin(df_bool, prop_vaccin_simul))
+print(estim_Vaccin(df_bool_vaccin, prop_vaccin_simul))
 
 # Fonction Estim_b0
-print(SimulationHPV.estim_b0.([0.05,0.06],[12.9,13]))
+print(estim_b0.([0.05,0.06],[12.9,13]))
 
 # Fonction Estim_HPV
 prop = SimulationHPV.borneSomme([16.5,70.9,7.2]/100,[20.7,75.5,9.4]/100)
 df_bool_HPV = DataFrame(rand(Bool, 100, 15),:auto)
-print(SimulationHPV.estim_HPV(df_bool_HPV,[0.4,0.2],6,0.05))
+print(estim_HPV(df_bool_HPV,[0.4,0.2],6,0.05))
 
 # Fonction datasets
 n = 1000
