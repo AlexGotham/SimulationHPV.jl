c93a7ab470a56191d30bdaa564be08280928dbc5
AlexGotham <aleger567@gmail.com>
2026-01-04 11:19:33 +0100

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -92,6 +92,7 @@ function datasets(n::Int64, target::Vector{Float64}, souche::Vector{String})
 
     # Simulation des vaccins en fonction des covariables
     SimulVaccin = estim_Vaccin(DF_bool, prop_vac)
+    DF.SimulVaccin = SimulVaccin
     DF_bool[!, :Vaccin] = SimulVaccin
 
     # Paramètres de l'âge
