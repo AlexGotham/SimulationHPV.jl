1074c137b98a2ebdfcb074646e31ddc1299b62cb
SergeSuuu <capitanserge73@gmail.com>
2026-01-19 11:18:16 +0100

    Fonction log linéaire

--- src/estim_Vaccin.jl
+++ src/estim_Vaccin.jl
@@ -19,4 +19,23 @@ function estim_Vaccin(df_bool::DataFrame, prop_vaccin::Float64)
     return Vaccin
 end
 
+function estim_Vaccin_log(df_bool::DataFrame, prop_vaccin::Float64)
+    W = [2.5,-0.5,1,2.5] # effet de l'age sur le vaccin
+    A = [- 0.5, 1, 1.5, 2, -0.5, 1.5, 2.5, -0.5, 1, 1.5, 2, 2.5] # coeff de confusion fixé
+    coef = vcat(W,A)
+    regression = Matrix(df_bool) * coef
 
+    # Calcul pour une prévalence 
+    β₀ = estim_b0_log.(prop_vaccin,regression)
+    prob_reg = exp.(β₀ .+ regression)
+
+    # Simulation des infections avec le rajout d'un intercept
+    Vaccin = rand.(Binomial.(1,prob_reg))
+
+    return Vaccin
+end
+
+
+df_bool_vaccin = DataFrame(rand(Bool, 100, 16),:auto)
+prop_vaccin_simul = 0.55
+print(estim_Vaccin_log(df_bool_vaccin, prop_vaccin_simul))
--- src/estim_b0.jl
+++ src/estim_b0.jl
@@ -11,4 +11,12 @@ function estim_b0(incidence::Float64, reg::Float64)
    return res
 end
 
+function estim_b0_log(incidence::Float64, reg::Float64)
+   function β₀(β) 
+    mean(exp.(reg .+ β)) - incidence
+   end
+   res = find_zero(β₀, [-20.0,0.0])
+   return res
+end
+
 
